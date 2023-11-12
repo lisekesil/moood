966432c1f61a24a2a5243dc5dc0e11ab6977698d
Wojciech Lisowicz <48571490+lisekesil@users.noreply.github.com>
2023-11-12 13:04:54 +0100

    autosave entry

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,11 +1,24 @@
 "use client";
 
+import { updateEntry } from "@/utils/api";
 import { useState } from "react";
+import { useAutosave } from "react-autosave";
 
 const Editor = ({ entry }) => {
   const [value, setValue] = useState(entry.content);
+  const [isSaving, setIsSaving] = useState(false);
+
+  useAutosave({
+    data: value,
+    onSave: async (_value) => {
+      setIsSaving(true);
+      const updated = await updateEntry(entry.id, _value);
+      setIsSaving(false);
+    },
+  });
   return (
     <div className="w-full h-full">
+      {isSaving && <div>...saving</div>}
       <textarea
         className="w-full h-full p-8 text-xl outline-none"
         value={value}
